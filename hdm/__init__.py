8694d9bd3093ba8b3637abe06373ffd02b25b61e
Marta Reinhardt <martinhareinhardt@gmail.com>
2026-08-29 14:50:02 -0300

    Remove introductory docstring from __init__.py
    Removed docstring describing HDM project.

--- hdm/__init__.py
+++ hdm/__init__.py
@@ -1,11 +1,5 @@
-"""
-HDM (High Dimensions Map)
-Um motor leve de projeção geométrica, indexação e rastreamento cinemático em altas dimensões.
-"""
-
 from .scanner import HDMScanner
 from .kinematics import HDMKinematics
 
 __version__ = "0.1.0"
 __all__ = ["HDMScanner", "HDMKinematics"]
-
