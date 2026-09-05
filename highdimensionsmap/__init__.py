9e4de72bbfa55589ff3042c58ce06f04f0c48cf9
Marta Reinhardt <martinhareinhardt@gmail.com>
2026-09-04 22:02:59 -0300

    Replace MotionNoiseTracker with RobustMotionTracker

--- highdimensionsmap/__init__.py
+++ highdimensionsmap/__init__.py
@@ -1,9 +1,10 @@
 from .scanner import HDMScanner
 from .kinematics import HDMKinematics
-from .tracker import MotionNoiseTracker
+from .tracker import RobustMotionTracker, RobustMotionTracker as MotionNoiseTracker
 from .core import hdm
 from .api import app
 from .tensor import TrajectoryMotionTensor
 
 __version__ = "0.1.0"
-__all__ = ["HDMScanner", "HDMKinematics", "MotionNoiseTracker", "hdm", "app"]
+__all__ = ["HDMScanner", "HDMKinematics", "MotionNoiseTracker", "RobustMotionTracker", "hdm", "app"]
+
